8749bf64d7357928c0ddcbea48920944e84b9f93
Fr000g <fr000g@icloud.com>
2023-11-01 15:05:43 +0000

    update commands shell script

--- maxlive_commands_v1.sh
+++ maxlive_commands_v1.sh
@@ -1,24 +1,49 @@
-sudo docker build -t dropdatabase233/account_service ./account_service
-sudo docker build -t dropdatabase233/analytics_service ./analytics_service
-sudo docker build -t dropdatabase233/authentication_service ./authentication_service
-sudo docker build -t dropdatabase233/authorization_service ./authorization_service
-sudo docker build -t dropdatabase233/content_storage_service ./content_storage_service
-sudo docker build -t dropdatabase233/payment_gateway_service ./payment_gateway_service
-sudo docker build -t dropdatabase233/payment_service ./payment_service
-sudo docker build -t dropdatabase233/personalization_service ./personalization_service
-sudo docker build -t dropdatabase233/rating_service ./rating_service
-sudo docker build -t dropdatabase233/stream_service ./stream_service
-sudo docker build -t dropdatabase233/subscription_service ./subscription_service
-sudo docker build -t dropdatabase233/upload_service ./upload_service
-sudo docker run -d -p 8000:8000 dropdatabase233/account_service 
-sudo docker run -d -p 3000:3000 dropdatabase233/analytics_service 
-sudo docker run -d -p 3001:3000 dropdatabase233/authentication_service 
-sudo docker run -d -p 3002:3000 dropdatabase233/authorization_service 
-sudo docker run -d -p 3003:3000 dropdatabase233/content_storage_service 
-sudo docker run -d -p 3004:3000 dropdatabase233/payment_gateway_service 
-sudo docker run -d -p 3005:3000 dropdatabase233/payment_service 
-sudo docker run -d -p 3006:3000 dropdatabase233/personalization_service 
-sudo docker run -d -p 3007:3000 dropdatabase233/rating_service 
-sudo docker run -d -p 3008:3000 dropdatabase233/stream_service 
-sudo docker run -d -p 3009:3000 dropdatabase233/subscription_service 
-sudo docker run -d -p 3010:3000 dropdatabase233/upload_service
+#!/bin/bash
+
+# check if image exist
+function check_image {
+  IMAGE_NAME=$1
+  if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
+    echo "image $IMAGE_NAME not exists, building..."
+    docker build -t $IMAGE_NAME ./$IMAGE_NAME
+  else
+    echo "image $IMAGE_NAME exists, skip build"
+  fi
+}
+
+# check if serve running
+function check_running {
+  SERVICE_NAME=$1
+  if [[ "$(docker ps -q --filter name=$SERVICE_NAME 2> /dev/null)" == "" ]]; then
+    echo "container $SERVICE_NAME not running, starting..."
+    docker run -d -p $2:3000 $SERVICE_NAME
+  else
+    echo "container $SERVICE_NAME already running, skip"
+  fi
+}
+
+check_image "dropdatabase233/account_service"
+check_image "dropdatabase233/analytics_service"
+check_image "dropdatabase233/authentication_service"
+check_image "dropdatabase233/authorization_service"
+check_image "dropdatabase233/content_storage_service"
+check_image "dropdatabase233/payment_gateway_service"
+check_image "dropdatabase233/payment_service"
+check_image "dropdatabase233/personalization_service"
+check_image "dropdatabase233/rating_service"
+check_image "dropdatabase233/stream_service"
+check_image "dropdatabase233/subscription_service"
+check_image "dropdatabase233/upload_service"
+
+check_running "account_service" 8000
+check_running "analytics_service" 3000
+check_running "authentication_service" 3001
+check_running "authorization_service" 3002
+check_running "content_storage_service" 3003
+check_running "payment_gateway_service" 3004
+check_running "payment_service" 3005
+check_running "personalization_service" 3006
+check_running "rating_service" 3007
+check_running "stream_service" 3008
+check_running "subscription_service" 3009
+check_running "upload_service" 3010
